8b1a9f4a184755b526fbf2a3550d638b6f0cd418
ArIonMark1 <65442269+ArIonMark1@users.noreply.github.com>
2023-07-28 08:19:42 +0000

    Deploying to gh-pages from @ ArIonMark1/goit-react-hw-08-phonebook@e60b8c2e19533247b31657ef68d2fb08336cc993 🚀

---
+++ static/js/206.5d7cd4a3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[206],{1206:function(e,t,n){n.r(t);var i=n(184),o={container:{minHeight:"calc(100vh - 150px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};t.default=function(){return(0,i.jsx)("div",{style:o.container,children:(0,i.jsx)("h2",{style:o.title,children:"Welcome to the phone book site :-)"})})}}}]);
+//# sourceMappingURL=206.5d7cd4a3.chunk.js.map
